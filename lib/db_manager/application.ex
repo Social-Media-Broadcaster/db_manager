8b4cc762777bf05147dd6652acb435d52ccdd31d
Andrii Yaremenko <yarrem@gmail.com>
2019-11-22 14:31:01 +0200

    fixes stupid mistake in app

--- lib/db_manager/application.ex
+++ lib/db_manager/application.ex
@@ -12,7 +12,7 @@ defmodule DbManager.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: EMapper.Supervisor]
+    opts = [strategy: :one_for_one, name: DbManager.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
